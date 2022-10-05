7a74bd6fdcf143def78d3fa6a7f2087674db0d23
cnxk <cos4338@gmail.com>
2022-10-05 21:45:20 +0900

    Feat: Handle login component

--- src/pages/AuthPage/components/AuthInput.tsx
+++ src/pages/AuthPage/components/AuthInput.tsx
@@ -1,15 +1,31 @@
+import useAuth from '../../../hooks/useAuth';
+
 const AuthInput = () => {
+  const { email, password, handleInputChange, handleLogin } = useAuth();
+
   return (
     <form>
       <label>
         email
-        <input type="email" placeholder="이메일을 입력해 주세요" />
+        <input
+          name="email"
+          type="email"
+          value={email}
+          placeholder="이메일을 입력해 주세요"
+          onChange={handleInputChange}
+        />
       </label>
       <label>
         password
-        <input type="password" placeholder="비밀번호를 입력해 주세요" />
+        <input
+          name="password"
+          type="password"
+          value={password}
+          placeholder="비밀번호를 입력해 주세요"
+          onChange={handleInputChange}
+        />
       </label>
-      <button>로그인</button>
+      <button onClick={handleLogin}>로그인</button>
     </form>
   );
 };
