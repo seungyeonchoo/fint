a372d1f27fdaf6204045d2a23193a3a9d67c5556
cnxk <cos4338@gmail.com>
2022-10-06 16:59:04 +0900

    Feat: Create common types module

---
+++ src/types/types.ts
@@ -0,0 +1,65 @@
+export interface Accounts {
+  id: number;
+  userId: number;
+  uuid: string;
+  broker_id: string;
+  status: number;
+  number: string;
+  name: string;
+  assets: string;
+  payments: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  user: User;
+}
+
+export interface Users {
+  id: number;
+  uuid: string;
+  photo: string;
+  name: string;
+  email: string;
+  age: number;
+  gender_origin: number;
+  birth_date: string;
+  phone_number: string;
+  address: string;
+  detail_address: string;
+  last_login: string;
+  created_at: string;
+  updated_at: string;
+  accounts: Account[];
+}
+
+interface Account {
+  id: number;
+  userId: number;
+  uuid: string;
+  broker_id: string;
+  status: number;
+  number: string;
+  name: string;
+  assets: string;
+  payments: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+}
+
+interface User {
+  id: number;
+  uuid: string;
+  photo: string;
+  name: string;
+  email: string;
+  age: number;
+  gender_origin: number;
+  birth_date: string;
+  phone_number: string;
+  address: string;
+  detail_address: string;
+  last_login: string;
+  created_at: string;
+  updated_at: string;
+}
