791c02704582153dca5396659248a9f45cbc1aae
cnxk <cos4338@gmail.com>
2022-10-07 01:28:12 +0900

    Feat: Create userListSlice to handle getting user list

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import accountListSlice from './Slices/accountListSlice';
+import userListSlice from './Slices/userListSlice';
 
 const rootReducer = combineReducers({
   accounts: accountListSlice,
+  users: userListSlice,
 });
 
 const store = configureStore({
