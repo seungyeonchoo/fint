08bf026989dd2f2c312878d0187425166faa021b
cnxk <cos4338@gmail.com>
2022-10-06 17:00:48 +0900

    Feat: Create module to handle getting users info

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,22 +1,21 @@
+import { Users } from '../types/types';
 import instance from './AxiosInstance';
-
-// export interface User {}
 
 // export interface UserSetting {}
 
 interface GetUsers {
-  getUserList: (params?: object) => Promise<any>;
-  getUserDetail: (id: string) => Promise<any>;
+  getUserList: (params?: object) => Promise<Array<Users>>;
+  getUserDetail: (id: string) => Promise<Users>;
   getUserSetting: () => Promise<any>;
 }
 
 class GetUserService implements GetUsers {
-  getUserList: (params?: object | undefined) => Promise<any> = async (params?) => {
-    const { data } = await instance.get('/users', { params: { ...params, _expand: 'user' } });
+  getUserList: (params?: object | undefined) => Promise<Array<Users>> = async (params?) => {
+    const { data } = await instance.get('/users', { params: { ...params, _embed: 'user' } });
     return data;
   };
-  getUserDetail: (id: string) => Promise<any> = async id => {
-    const { data } = await instance.get(`/users/${id}`, { params: { _expand: 'user' } });
+  getUserDetail: (id: string) => Promise<Users> = async id => {
+    const { data } = await instance.get(`/users/${id}`, { params: { _embed: 'user' } });
     return data;
   };
   getUserSetting: () => Promise<any> = async () => {
