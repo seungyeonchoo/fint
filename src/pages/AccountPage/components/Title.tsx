1c9bd77e3b50d8418591a250725944812abd4853
cnxk <cos4338@gmail.com>
2022-10-13 16:56:20 +0100

    Feat: Add account list title

--- src/pages/AccountPage/components/Title.tsx
+++ src/pages/AccountPage/components/Title.tsx
@@ -1,5 +1,19 @@
+import { List, Row } from '../../UserPage/components/UserList';
+
 const Title = () => {
-  return <div>title</div>;
+  return (
+    <List>
+      <Row rows={9}>고객명</Row>
+      <Row rows={9}>브로커명</Row>
+      <Row rows={9}>계좌번호</Row>
+      <Row rows={9}>계좌상태</Row>
+      <Row rows={9}>계좌명</Row>
+      <Row rows={9}>평가금액</Row>
+      <Row rows={9}>입금금액</Row>
+      <Row rows={9}>계좌활성화여부</Row>
+      <Row rows={9}>계좌개설일</Row>
+    </List>
+  );
 };
 
 export default Title;
