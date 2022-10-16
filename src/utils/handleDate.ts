6c08f72a27c581cb8ec81643c30be04fcfd09921
cnxk <cos4338@gmail.com>
2022-10-16 21:27:36 +0100

    Feat: Add util function to handle date form

--- src/utils/handleDate.ts
+++ src/utils/handleDate.ts
@@ -1,7 +1,7 @@
 const handleDateForm: (input: string) => string = input => {
   const date = new Date(input);
   const handleYear = date.getFullYear();
-  const handleMonth = date.getMonth() + 1 < 10 ? `0${date.getMonth()}` : date.getMonth();
+  const handleMonth = date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth();
   const handleDate = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
 
   return `${handleYear}년 ${handleMonth}월 ${handleDate}일`;
